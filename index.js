2474df66d4db9a8c17e56fede1208035cf1d6fc1
dvasuta1 <37503329+dvasuta1@users.noreply.github.com>
2024-06-06 12:00:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT })
 wss.on('connection', ws => {
   ws.on('message', message => {
+    console.log(wss.clients);
     console.log(`Received message => ${message}`)
   })
   ws.send('Hello! Message From Server!!')
