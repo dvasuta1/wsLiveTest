0f5636493fb7582e2519222e43f7f62b32ca8b37
dvasuta1 <37503329+dvasuta1@users.noreply.github.com>
2024-06-06 10:10:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+/*import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8088 });
 
@@ -9,4 +9,13 @@ wss.on("connection", function connection(ws) {
 
   ws.send("something");
 });
-console.log("The WebSocket server is running on port 8088");
+console.log("The WebSocket server is running on port 8088");*/
+const WebSocket = require('ws')
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT })
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
