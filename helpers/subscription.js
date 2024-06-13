07bffaeb57b6e31c0dd58c687e22e6628c5245e2
Dmitry Vasyuta <dmitry.vasyuta@playtech.com>
2024-06-13 19:13:12 +0300

    check addSubsAndCorrelationProps

--- helpers/subscription.js
+++ helpers/subscription.js
@@ -1,6 +1,8 @@
-const subscribeResponceMessage = require("../data/subscribe/subscribeResponce.js");
+//const subscribeResponceMessage = require("../data/subscribe/subscribeResponce.js");
+const subscribeResponceMessage = require("../data/subscribe/subscribeResponce");
 
 const addSubsAndCorrelationProps = (data, correlationId, subscriptionId) => {
+  console.log("data addSubsAndCorrelationProps", data);
   let subscribeResponceMessageClone = JSON.parse(JSON.stringify(data));
   subscribeResponceMessageClone.correlationId = correlationId;
   subscribeResponceMessageClone.subscribeResponse.subscriptionId = subscriptionId;
