680c0d514882cb7269131ad338a4325d07f8eb51
Dmitry Vasyuta <dmitry.vasyuta@playtech.com>
2024-06-18 15:07:16 +0300

    fix file pathes

--- helpers/config.js
+++ helpers/config.js
@@ -1,7 +1,7 @@
 const dataSetMap = {
-  defaultData: "../data/update/default.json",
-  1000: "../data/update/1000.json",
-  2000: "../data/update/2000.json",
+  defaultData: "./data/update/default.json",
+  1000: "./data/update/1000.json",
+  2000: "./data/update/2000.json",
 };
 
 module.exports = { dataSetMap };
