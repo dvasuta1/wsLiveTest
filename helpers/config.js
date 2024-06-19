7be027431076d45f13e72bc1ca2ea526eebd48a7
Dmitry Vasyuta <dmitry.vasyuta@playtech.com>
2024-06-19 12:41:39 +0300

    add test.json

--- helpers/config.js
+++ helpers/config.js
@@ -2,6 +2,7 @@ const dataSetMap = {
   defaultData: "./data/update/default.json",
   1000: "./data/update/1000.json",
   2000: "./data/update/2000.json",
+  test: "./data/update/test.json",
 };
 
 module.exports = { dataSetMap };
