7383bbd42df0463566e098191017f8421614cb57
Dmitry Vasyuta <dmitry.vasyuta@playtech.com>
2024-06-25 11:34:02 +0300

    add spinawin.json

--- helpers/config.js
+++ helpers/config.js
@@ -3,6 +3,7 @@ const dataSetMap = {
   1000: "./data/update/1000.json",
   2000: "./data/update/2000.json",
   test: "./data/update/test.json",
+  spinawin: "./data/update/spinawin.json",
 };
 
 module.exports = { dataSetMap };
