c5ec970c76f75306da6b5ae676a3026869129c7e
dvasuta1 <37503329+dvasuta1@users.noreply.github.com>
2025-08-04 15:01:33 +0300

    Update config.js

--- helpers/config.js
+++ helpers/config.js
@@ -21,8 +21,8 @@ const dataConfig = {
   },
   betcaliente: { 
     defaultData: {
-      s: "./data/subscribe/betcaliente/subscribe1001.json",
-      u: "./data/update/betcaliente/1001.json"
+      s: "./data/subscribe/betcaliente/subscribe.json",
+      u: "./data/update/betcaliente/default.json"
     },
     1001: {
       s: "./data/subscribe/betcaliente/subscribe1001.json",
